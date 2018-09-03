392473757cd78482532c7f6bb99acd673a3edc03
MrCheeze <fishycheeze@yahoo.ca>
2018-09-03 11:54:59 -0400

    water heightmap

---
+++ heightmap_water.py
@@ -0,0 +1,53 @@
+from PIL import Image, ImageDraw
+import os
+import math
+
+img = Image.new("HSV", (0x40*6*2, 0x40*6*2))
+
+i = 0
+for folder, _, files in os.walk('terrain_water'):
+    if folder == 'terrain_water':
+        continue
+    y_high = [0,0,0,1,2,1,
+              1,2,2,0,0,0,
+              1,1,2,2,1,2,
+              3,4,3,3,4,4,
+              5,5,5,3,3,4,
+              4,3,4,5,5,5][i] * 0x80
+    x_high = [0,1,2,0,0,1,
+              2,1,2,3,4,5,
+              3,4,3,4,5,5,
+              0,0,1,2,1,2,
+              0,1,2,3,4,3,
+              4,5,5,3,4,5][i] * 0x80
+    
+    for file in files:
+        tile = int(file[-13:-11],0x10)%4
+        if tile == 0:
+            x_mid = 0
+            y_mid = 0
+        elif tile == 1:
+            x_mid = 0x40
+            y_mid = 0
+        elif tile == 2:
+            x_mid = 0
+            y_mid = 0x40
+        elif tile == 3:
+            x_mid = 0x40
+            y_mid = 0x40
+        print(file)
+        f = open(folder+'/'+file, 'rb')
+        for y in range(y_high + y_mid, y_high + y_mid + 0x40):
+            for x in range(x_high + x_mid, x_high + x_mid + 0x40):
+                height = int.from_bytes(f.read(2),'little') // 256
+                x_flow = int.from_bytes(f.read(2),'little') - 0x8000
+                y_flow = int.from_bytes(f.read(2),'little') - 0x8000
+                f.read(1)
+                material = int.from_bytes(f.read(1),'little') * 32
+                angle = int(math.atan2(y_flow, x_flow) * 128/math.pi + 128)
+                magnitude = int(math.hypot(x_flow, y_flow) / 182) # chosen based on maximum flow that exists in practice
+                img.putpixel((x,y), (height, 255, 255))
+        f.close()
+    i += 1
+img.convert("RGB").save('heightmap_water.png')
+            
